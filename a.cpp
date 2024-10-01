fc89db8a885a0c19435e8f9289d59d1b148bbd0c
AnushkaPankhuri <anushka.pat.bih@gmail.com>
2024-10-01 20:01:26 +0530

    hello this is 2nd commit

--- a.cpp
+++ a.cpp
@@ -1,1 +1,11 @@
-dxbjbydd
+#include <iostream>
+int main(){
+    std::cout <<"hello world 123!" << std::endl;
+    std::cout <<"hi world 123!" << std::endl;
+    std::cout <<"nihao world 123!" << std::endl;
+    std::cout <<"namaste world 123!" << std::endl;
+    
+    
+
+}
+
